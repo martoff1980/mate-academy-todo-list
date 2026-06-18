7be1cdde6d71f0623a3d65898b8c5d8c41814644
martoff <martoff@ukr.net>
2026-06-18 18:50:59 +0300

    Final version Todo List project

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
@@ -12,12 +13,12 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     is_done = models.BooleanField(default=False)
-    tags = models.ManyToManyField(Tag, related_name='tasks')
+    tags = models.ManyToManyField(Tag, related_name="tasks")
 
     class Meta:
         # Сортировка: сначала невыполненные (False), затем выполненные (True)
         # Внутри этих групп — от самых новых к старым
-        ordering = ['is_done', '-created_at']
+        ordering = ["is_done", "-created_at"]
 
     def __str__(self):
         return f"{self.content[:30]}... (Done: {self.is_done})"
--- todo/urls.py
+++ todo/urls.py
@@ -1,22 +1,35 @@
 from django.urls import path
 from .views import (
-    TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, toggle_task_status,
-    TagListView, TagCreateView, TagUpdateView, TagDeleteView
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    toggle_task_status,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
 )
 
-app_name = 'todo'
+app_name = "todo"
 
 urlpatterns = [
     # Tasks
-    path('', TaskListView.as_view(), name='index'),
-    path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
-    path('tasks/<int:pk>/update/', TaskUpdateView.as_view(), name='task-update'),
-    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
-    path('tasks/<int:pk>/toggle/', toggle_task_status, name='task-toggle'),
-    
+    path("", TaskListView.as_view(), name="index"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(), name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+    path("tasks/<int:pk>/toggle/", toggle_task_status, name="task-toggle"),
     # Tags
-    path('tags/', TagListView.as_view(), name='tag-list'),
-    path('tags/create/', TagCreateView.as_view(), name='tag-create'),
-    path('tags/<int:pk>/update/', TagUpdateView.as_view(), name='tag-update'),
-    path('tags/<int:pk>/delete/', TagDeleteView.as_view(), name='tag-delete'),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]

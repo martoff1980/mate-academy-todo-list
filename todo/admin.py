739360b2c45a77d07b23deffdbe5cb01ee68794e
martoff <martoff@ukr.net>
2026-06-19 19:09:15 +0300

    Add admin.py, now  managed through the Django admin panel

---
+++ todo/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from todo.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    # Fields that will be displayed in the task list table
+    list_display = ('content', 'created_at', 'deadline', 'is_done')
+    
+    # Fields by which you can filter tasks in the right panel
+    list_filter = ('is_done', 'tags')
+    
+    # Fields by which the search will work 
+    # (the __ symbol means searching by 
+    # the name field of the associated Tag model)
+    search_fields = ('content', 'tags__name')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    
+    search_fields = ('name',)
